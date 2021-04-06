93ed506cdd07173d0460d2734c76375e3419e0de
ArnoldPat <81981666+ArnoldPat@users.noreply.github.com>
2021-04-06 16:32:36 +1000

    Add files via upload

--- src/test/java/jupiter/test/TestCase3.java
+++ src/test/java/jupiter/test/TestCase3.java
@@ -11,39 +11,37 @@ import org.testng.annotations.Test;
 import org.testng.asserts.*;
 import static org.testng.Assert.assertTrue;
 
-public class TestCase2 {
+public class TestCase3 {
 
 	WebDriver driver = new ChromeDriver();
-	
+
 	public void submitContact() {
-		
+
 		// Prime the web browser and Contact Form page for testing.
 		driver.get("https://jupiter.cloud.planittesting.com/#/");
 		WebDriverWait wait = new WebDriverWait(driver,30);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Contact")));
 		WebElement contact = driver.findElement(By.linkText("Contact"));
 		contact.click();
-		
-		// Populate Mandatory fields and validate on submit
+
+		// Populate Mandatory fields with invalid data, and validate on submit
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='forename']")));
 		WebElement fnametxt = driver.findElement(By.xpath("//input[@id='forename']"));
 		WebElement emailtxt = driver.findElement(By.xpath("//input[@id='email']"));
 		WebElement msgtxt = driver.findElement(By.xpath("//input[@id='message']"));
-		
-		fnametxt.sendKeys("John");
-		emailtxt.sendKeys("john.dee@monad.com");
-		msgtxt.sendKeys("Lorem Ipsum");
-		
+
+		fnametxt.sendKeys("$@#252$");
+		emailtxt.sendKeys("123%@#$");
+		msgtxt.sendKeys("@#$@#$");
+
 		WebElement submit = driver.findElement(By.xpath("//*[.='Submit']"));
 		submit.click();
-		
+
 		// Check if successful submission message is displayed.
 		validateMandatory();
 	}
 	
 	private void validateMandatory() {
-		assertTrue("Verification succesful. Form submitted successfully.", driver.findElement(By.xpath("//strong[contains(text(),'Thanks')]")).isEnabled());
-		System.out.println("Passed");
+		assertTrue("Verification failed. Invalid characters did not return error.", driver.findElement(By.xpath("//strong[contains(text(),'Thanks')]")).isEnabled());
 	}
-	
 }
