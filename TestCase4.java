8b43c58acb8cfdf603496e6a30571684fe0f05b0
ArnoldPat <81981666+ArnoldPat@users.noreply.github.com>
2021-04-06 16:41:00 +1000

    Add files via upload

---
+++ TestCase4.java
@@ -0,0 +1,36 @@
+package jupiter.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.*;
+import static org.testng.Assert.assertTrue;
+
+public class TestCase4 {
+
+	WebDriver driver = new ChromeDriver();
+
+	public void submitContact() {
+		
+		// Prime the web browser and Contact Form page for testing.
+		driver.get("https://jupiter.cloud.planittesting.com/#/");
+		WebDriverWait wait = new WebDriverWait(driver,30);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Shop")));
+		WebElement shop = driver.findElement(By.linkText("Shop"));
+		shop.click();
+		
+		//Purchase Funny Cow(qty:2) and Fluffy Bunny(qty: 1)
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[@id='product-6']//p/a")));
+		driver.findElement(By.xpath("//li[@id='product-6']//p/a")).click();
+		driver.findElement(By.xpath("//li[@id='product-6']//p/a")).click();
+		
+		driver.findElement(By.xpath("//li[@id='product-4']//p/a")).click();
+	}
+
+	
+}
